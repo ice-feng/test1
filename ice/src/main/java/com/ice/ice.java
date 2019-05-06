c910b9f0657743c2ee337f092d0d7b73cef74fcf
shilijiegh <835466742@qq.com>
2019-05-06 09:30:05 +0800

    liquid

--- ice/src/main/java/com/ice/ice.java
+++ ice/src/main/java/com/ice/ice.java
@@ -5,8 +5,6 @@ public class ice {
 		System.out.println("ice");
 		System.out.println("feng");
 		System.out.println("wind");
-		System.out.println("crystal");
 		System.out.println("liquid");
-
 	}
 }
